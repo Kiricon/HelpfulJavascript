61990bbd4e613d714b75e1c6c3fb2985744c033b
Dominic Valenciana <kiricon@live.com>
2016-11-30 12:31:54 -0700

    Fixed is IE method

--- BrowserHelpers/IsIE.js
+++ BrowserHelpers/IsIE.js
@@ -2,11 +2,11 @@ function isIE() {
     var ua = window.navigator.userAgent;
     var msie = ua.indexOf("MSIE ");
 
-    if (msie > 0)
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
     {
         return true;
     }
-    else  
+    else  // If another browser, return 0
     {
         return false;
     }
